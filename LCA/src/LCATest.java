0f5aa1e0a75e0ae71e701db96becd5de2c77bd38
fieldsal <32303026+fieldsal@users.noreply.github.com>
2018-09-30 20:59:53 +0100

    LCA Test class added with node class test

---
+++ LCA/src/LCATest.java
@@ -0,0 +1,22 @@
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertNull;
+
+import org.junit.Test;
+import org.junit.Ignore;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public class LCATest {
+	//Testing Node class
+	@Test
+	public void testNode() {
+		
+		 new Node(4);
+		 new Node(8);
+	}
+	
+	
+}
